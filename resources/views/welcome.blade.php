eb99644eaf26044fc7d5a344daf392ee9400c209
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2020-08-02 18:42:46 +0900

    modify-title-welcome-blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Potfy.me
                 </div>
 
                 <div class="links">
