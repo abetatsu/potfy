fef5f1ff4be96dd189b76460dfe47765c3a44c20
uoyaryota <lestwinsdance8@gmail.com>
2020-08-06 18:46:04 +0900

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>POTFY</title>
+        <title>POTFY LOCAL</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
