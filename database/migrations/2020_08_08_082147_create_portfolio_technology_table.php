16d4364d3c6e409679b83debc1002cd30f897178
uoyaryota <lestwinsdance8@gmail.com>
2020-08-08 19:44:04 +0900

    new migrations

--- database/migrations/2020_08_08_082147_create_portfolio_technology_table.php
+++ database/migrations/2020_08_08_082147_create_portfolio_technology_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePortfolioTechnologiesTable extends Migration
+class CreatePortfolioTechnologyTable extends Migration
 {
     /**
      * Run the migrations.
