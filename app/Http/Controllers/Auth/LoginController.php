bef702eef626babc3b167353e2e911138353d6d1
yusan12 <haraheri7212@yahoo.co.jp>
2020-08-15 18:02:40 +0900

    プルリク修正2

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -66,7 +66,8 @@ class LoginController extends Controller
                 'token'     => $user->token,
                 'name'      => $user->name,
                 'email'     => $user->email,
-                'image'    => $user->image,
+                'image'     => $user->image,
+                'image'     => str_replace('http://','https://',$user->avatar),
             ]);
             Auth::login($socialUser, true);
         } catch (Exception $e) {
