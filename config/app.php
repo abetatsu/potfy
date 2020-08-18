2971ce5bdb503420caa668df79f66e6f5e8a4121
tsubasa tanabe <54805462+t-aburasoba@users.noreply.github.com>
2020-08-18 12:55:33 +0900

    Update app.php

--- config/app.php
+++ config/app.php
@@ -174,9 +174,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        
+
         Laravel\Socialite\SocialiteServiceProvider::class,
-        
+
     ],
 
     /*
