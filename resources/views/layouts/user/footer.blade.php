671abcc85b21badb0735d39d966508bedea0cce0
masagao <masanao@gaogao.asia>
2020-09-05 11:00:11 +0900

    Add footer layouts

---
+++ resources/views/layouts/user/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="bg-secondary text-white text-center py-4">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <small>©Copyright 2020 GAOGAO GATE . All Rights Reserved.</small>
+          </div>
+          <div class="col-md-8">
+            <ul class="row">
+              <li class="col-md-3 small"><a href="" target="_blank">利用規約</a></li>
+              <li class="col-md-3 small"><a href="" target="_blank">プライバシーポリシー</a></li>
+              <li class="col-md-3 small"><a href="mailto:masanao@gaogao.asia?subject=Potfy お問い合わせ" target="_blank">お問い合わせ</a></li>
+              <li class="col-md-3 small"><a href="" target="_blank">運営</a></li>
+            </ul>
+          </div>
+        </div>
+    </div>
+</footer>
