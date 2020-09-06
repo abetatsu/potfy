a5317412ae59955c7e4669627035509961f3a494
masagao <masanao@gaogao.asia>
2020-09-06 20:08:25 +0900

    Add favicons and update favicon design

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,6 +7,13 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/image/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/image/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/image/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('assets/image/site.webmanifest') }}">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
     <!-- Scripts -->
     <script src="{{ asset('assets/js/app.js') }}" defer></script>
 
