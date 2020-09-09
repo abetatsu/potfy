342dc20bb6b19b4af0703e10e4a6fe86ecf6e4c8
yusan12 <haraheri7212@yahoo.co.jp>
2020-09-09 17:58:03 +0900

    nameのバリデーションを削除した

--- app/Http/Controllers/User/Auth/RegisterController.php
+++ app/Http/Controllers/User/Auth/RegisterController.php
@@ -64,7 +64,6 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8'],
         ]);
