a73876f4b231ecac8f729072c200bd11e55b1525
Masanao Suzuki <30583780+masagao@users.noreply.github.com>
2020-09-13 21:39:01 +0900

    Update CommentController.php

--- app/Http/Controllers/User/CommentController.php
+++ app/Http/Controllers/User/CommentController.php
@@ -48,7 +48,6 @@ class CommentController extends Controller
 
         $comment->save();
         
-
         return redirect()->route('user.portfolios.show', $portfolio->id)->with('success', 'コメント完了しました。')->with('comment', $comment->id);
     }
 
