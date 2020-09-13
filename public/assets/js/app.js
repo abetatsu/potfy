0ebf21511ec3c71ac678dea39f053d17a5624479
Ko Inoue <k.inoue@remcorp.jp>
2020-09-13 18:43:54 +0900

    cardの高さ統一、UI変更

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -50065,8 +50065,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/suzukimasanao/dev/potfy/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/suzukimasanao/dev/potfy/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/koinoue/docker-lamp-master/code/potfy/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/koinoue/docker-lamp-master/code/potfy/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/user/profiles/show.blade.php
+++ resources/views/user/profiles/show.blade.php
@@ -26,18 +26,17 @@
             <div class="row text-center">
                 @if (count($portfolios) !== 0)
                 @foreach ($portfolios as $portfolio)
-                <div class="col-xl-3 col-md-6 mb-3">
-                    <div class="card shadow transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                <div class="col-xl-4 col-md-6 mb-3">
+                    <div class="card shadow transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105" style="height: 385px;">
                         <a href="{{route('user.portfolios.show',$portfolio->id)}}" class="no-underline hover:no-underline">
                             <img class="card-img-top object-cover h-48 w-full" src="{{ isset($portfolio->image_path) ? $portfolio->image_path : 'https://res.cloudinary.com/dlalfv68e/image/upload/v1598249615/v8ycx2qljsz6u4lzcosm.png' }}" alt="画像の登録はありません">
                             <div class="card-body pb-0">
-                                    <h5 class="card-title ">タイトル：{{ $portfolio->title }}</h5>
-                                    <p class="card-text">内容：{{ $portfolio->description }}</p>
-                                    <p class="card-text">リンク：{{ $portfolio->link }}</p>
+                                <h5 class="card-title font-extrabold text-left">{{ RoundSentence($portfolio->title, 19, 18) }}</h5>
+                                <p class="card-text text-left" style="height: 90px">{!! RoundSentence($portfolio->description, 74, 75) !!}</p>
                             </div>
                         </a>
-                        <a href="{{ route('user.users.show', $portfolio->user_id) }}" class="d-flex justify-content-center align-items-center">
-                            <img src="{{ $portfolio->user->image }}" alt="" class="rounded-circle h-10 p-2">
+                        <a href="{{ route('user.users.show', $portfolio->user_id) }}" class="d-flex justify-content-end align-items-center pr-4 my-2">
+                            <img src="{{ ($portfolio->user->image)?$portfolio->user->image: '/assets/image/android-chrome-192x192.png' }}" alt="" class="rounded-circle h-10 p-2">
                             <h3 class="card-text">{{ $portfolio->user->name }}</h3>
                         </a>
                     </div>
