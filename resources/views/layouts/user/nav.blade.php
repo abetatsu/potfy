38c76ab291dfca17d41738eb27b26e2805df6a59
uoyaryota <lestwinsdance8@gmail.com>
2020-09-15 15:54:06 +0800

    add_auth

--- resources/views/layouts/user/nav.blade.php
+++ resources/views/layouts/user/nav.blade.php
@@ -15,8 +15,9 @@
 
 			<!-- Right Side Of Navbar -->
 			<ul class="navbar-nav ml-auto">
+			@auth
 				<a href="{{ route('user.portfolios.create')}}" class="btn bg-potfyYellow hover:bg-potfyYellowTitle text-white font-bold py-2 px-4 rounded">新規投稿</a>
-
+      @endauth
 				<!-- Authentication Links -->
 				@unless (Auth::guard('user')->check())
 					<li class="nav-item">
